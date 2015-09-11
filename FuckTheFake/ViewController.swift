5ccdc8933e34e79970654be25466216d7883f57e
Peigen.Liu <peigen.liu@qunar.com>
2015-09-11 13:34:36 +0800

    Initial Commit

---
+++ FuckTheFake/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FuckTheFake
+//
+//  Created by Parker on 9/11/15.
+//  Copyright © 2015 Parker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
